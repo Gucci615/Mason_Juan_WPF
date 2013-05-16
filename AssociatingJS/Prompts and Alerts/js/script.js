001e7463687988c84d878e0fb9931d1a13cc407a
Juan Mason <jemason@fullsail.edu>
2013-05-16 11:03:19 -0400

    Prompts & Alerts Index & JS files

---
+++ AssociatingJS/Prompts and Alerts/js/script.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+//Prompts and Alerts
+
+//var userInput = prompt("What Is you problem?");
+//console.log(userInput);
+
+var width = prompt(" Enter width of triangle:");//user input width
+var highth = prompt("Enter height");//User input height
+var area =  width * highth; //Calculate area
+alert("Area of this triangle is:" + area);//Display area message on screen
+console.log(area);
