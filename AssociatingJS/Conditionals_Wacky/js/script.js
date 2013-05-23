51d03bd345afe6030dd9b0a64d4afd683f532fb4
Juan Mason <jemason@fullsail.edu>
2013-05-23 00:23:20 -0400

    Add comment notes

--- AssociatingJS/Conditionals_Wacky/js/script.js
+++ AssociatingJS/Conditionals_Wacky/js/script.js
@@ -1,12 +1,17 @@
 // Conditionals Wacky
 //Find the seconday color for you and your mate
 
+//Select your color
 var whatsYourColor = prompt("Select your favorit primary color form the list: \n Red \n Blue \n Yellow","Red");
 
+//Store your color
 var yourColor = whatsYourColor;
 alert(yourColor);
 
+//Select your mates color
 var whatsMatesColor = prompt("Select your favorit primary color form the list: \n Red \n Blue \n Yellow","Red");
 
+//Store your mates color
 var matesColor = whatsMatesColor;
 alert(matesColor);
+
