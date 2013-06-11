dd8a3e6d84852cfc287fa6a359e1c34975363a62
Juan Mason <jemason@fullsail.edu>
2013-06-10 23:54:19 -0400

    Mason Juan Dog Age Initial JS

---
+++ AssociatingJS/Expressions_worksheet/Mason_Juan_Dog_Years/js/script.js
@@ -0,0 +1,8 @@
+//Mason_Juan_Dog_Age 06/10/13
+// Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in “dog years.”  Calculate how old Sparky the pit bull is in dog years based on his actual age.
+//     Givens:
+//Sparky’s age.
+//     Result Varable:
+//Sparky’s age in dog years.
+//    Result to Print:
+//“Sparky is X human years old which is X in dog years.
---
+++ AssociatingJS/Expressions_worksheet/js/script.js
@@ -0,0 +1,7 @@
+//Juan E. Mason 05/15/13 Dog Years
+// JavaScript Document
+
+var dogYear
+var dogAge = prompt("Enter the dogs age in human years",5);
+dogYear = dogAge * 7;
+alert(dogYear);
