2894e57f73d689020100f372ba52480cbc29eebe
Juan Mason <jemason@fullsail.edu>
2013-06-13 19:40:15 -0400

    add names of wrestlers to the array

--- AssociatingJS/Expressions_Wacky/js/script.js
+++ AssociatingJS/Expressions_Wacky/js/script.js
@@ -7,10 +7,15 @@
 
 alert("Take this quiz to ID the wrestler in you!"); 
 var  myArray = new Array();
+myArray[0] = "149 lbs or less"
 myArray[1] = "150 to 199 lbs";
 myArray[2] = "200 to 349 lbs";
 myArray[4] = "350 + lbs";
-myArray[5] = "High Flyer";
-myArray[6] = "Wrestling Technician";
-myArray[7] = "Ring General";
-myArray[8] = "Power House";
+myArray[5] = "Light Weight"
+myArray[6] = "High Flyer";
+myArray[7] = "Wrestling Technician & Power House";
+myArray[8] = "Super Heavy Weight";
+myArray[9] = "Hornswoggle, Dink the Clown, and El Torito";
+myArray[10] = "Rey Mysterio Jr, Evan Bourne, and Brian Kendrick";
+myArray[11] = "Bret Hart, Chris Benoit, The Undertaker, and Kurt Angle";
+myArray[12] = "Andre The Giant, Big Show, Mark Henry, and King Kong Bundy";
