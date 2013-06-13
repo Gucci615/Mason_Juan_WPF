cd6f5ff8ae4174074a1a043915b2c750ee8f5d09
Juan Mason <jemason@fullsail.edu>
2013-06-13 14:48:08 -0400

    Remove some of the redundant parseFloat commands

--- AssociatingJS/Expressions_Industry/js/script.js
+++ AssociatingJS/Expressions_Industry/js/script.js
@@ -4,15 +4,15 @@
 //Video,Phone, Internet
 
 var videoPrice = prompt("How mush do you pay for video per month?", 59.99);
-var yourVideoPrice = parseFloat(videoPrice);
+var yourVideoPrice = videoPrice;
 console.log(yourVideoPrice);
 
 var phonePrice = prompt("How mush do you pay for phone per month?", 49.99);
-var yourPhonePrice = parseFloat(phonePrice);
+var yourPhonePrice = phonePrice;
 console.log(yourPhonePrice);
 
 var internetPrice = prompt("How mush do you pay for internet per month?", 24.99);
-var yourInternetPrice = parseFloat(internetPrice);
+var yourInternetPrice = internetPrice;
 console.log(yourInternetPrice);
 
 var starterTP = 99.00;
@@ -34,5 +34,7 @@ console.log("You price for video and phone service is $" + (parseFloat(yourInter
 // Add all 3 service prices together to get a total
 console.log("You price for all three services is $" + (parseFloat(yourVideoPrice) + parseFloat(yourPhonePrice) + parseFloat(yourInternetPrice)));
 
+//Diff between your price for all three and starterTP
+console.log("The diffrencei between your price for all three and Starter TriPlay is $" + (parseFloat(yourVideoPrice) + parseFloat(yourPhonePrice)) - parseFloat(starterTP));
 
 
