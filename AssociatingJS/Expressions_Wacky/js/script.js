e1a2d8b852dbf25b355046427314a3e841b61cbe
Juan Mason <jemason@fullsail.edu>
2013-06-13 19:12:07 -0400

    Define Array for weight

--- AssociatingJS/Expressions_Wacky/js/script.js
+++ AssociatingJS/Expressions_Wacky/js/script.js
@@ -6,5 +6,9 @@
 //What type of wrestler are you?
 
 alert("Take this quiz to ID the wrestler in you!"); 
-
+var  myArray = new Array();
+myArray[0] = "149 lbs or less";
+myArray[1] = "150 to 199 lbs";
+myArray[2] = "200 to 349 lbs";
+myArray[4] = "350 + lbs";
 
