5b2ff3f852f1f975f734bfc2fe0035132fb1272f
Juan Mason <jemason@fullsail.edu>
2013-06-13 14:34:31 -0400

    Add in Triple Play (TP) Var

--- AssociatingJS/Expressions_Industry/js/script.js
+++ AssociatingJS/Expressions_Industry/js/script.js
@@ -15,6 +15,13 @@ var internetPrice = prompt("How mush do you pay for internet per month?", 24.99)
 var yourInternetPrice = parseFloat(internetPrice);
 console.log(yourInternetPrice);
 
+var starterTP = 99.00;
+var prefTP = 109.99;
+var prefHDTP = 119.99;
+var prefPlusHDTP = 139.99;
+var primerTP = 159.99;
+
+
 //Add video & phone price
 console.log("You price for video and phone service is $" + (parseFloat(yourVideoPrice) + parseFloat(yourPhonePrice)));
 
