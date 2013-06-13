3b641a1c3583b4b037652af38e861f9e96cc91d6
Juan Mason <jemason@fullsail.edu>
2013-06-13 14:27:59 -0400

    Add price of internet and phone

--- AssociatingJS/Expressions_Industry/js/script.js
+++ AssociatingJS/Expressions_Industry/js/script.js
@@ -3,15 +3,15 @@
     //Expressions Industry
 //Video,Phone, Internet
 
-var videoPrice = prompt("How mush do you pay for video per month?", 59.95);
+var videoPrice = prompt("How mush do you pay for video per month?", 59.99);
 var yourVideoPrice = parseFloat(videoPrice);
 console.log(yourVideoPrice);
 
-var phonePrice = prompt("How mush do you pay for phone per month?", 49.95);
+var phonePrice = prompt("How mush do you pay for phone per month?", 49.99);
 var yourPhonePrice = parseFloat(phonePrice);
 console.log(yourPhonePrice);
 
-var internetPrice = prompt("How mush do you pay for internet per month?", 24.95);
+var internetPrice = prompt("How mush do you pay for internet per month?", 24.99);
 var yourInternetPrice = parseFloat(internetPrice);
 console.log(yourInternetPrice);
 
@@ -20,6 +20,9 @@ console.log("You price for video and phone service is $" + (parseFloat(yourVideo
 
 //Add video & internet price
 console.log("You price for video and phone service is $" + (parseFloat(yourVideoPrice) + parseFloat(yourInternetPrice)));
+
+//Add internet & phone price
+console.log("You price for video and phone service is $" + (parseFloat(yourInternetPrice) + parseFloat(yourPhonePrice)));
 
 // Add all 3 service prices together to get a total
 console.log("You price for all three services is $" + (parseFloat(yourVideoPrice) + parseFloat(yourPhonePrice) + parseFloat(yourInternetPrice)));
