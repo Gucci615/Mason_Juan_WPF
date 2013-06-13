726abb2621da787e9b2e0e54ae029ac3f5056ba0
Juan Mason <jemason@fullsail.edu>
2013-06-13 19:17:44 -0400

    Add wrestling styles to the array

--- AssociatingJS/Expressions_Wacky/js/script.js
+++ AssociatingJS/Expressions_Wacky/js/script.js
@@ -7,8 +7,10 @@
 
 alert("Take this quiz to ID the wrestler in you!"); 
 var  myArray = new Array();
-myArray[0] = "149 lbs or less";
 myArray[1] = "150 to 199 lbs";
 myArray[2] = "200 to 349 lbs";
 myArray[4] = "350 + lbs";
-
+myArray[5] = "High Flyer";
+myArray[6] = "Wrestling Technician";
+myArray[7] = "Ring General";
+myArray[8] = "Power House";
