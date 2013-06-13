e370db524426a881e6d8d9be29985e42669dc569
Juan Mason <jemason@fullsail.edu>
2013-06-13 15:47:56 -0400

    Prompt # of weeks ordered. Add additional comments.

--- AssociatingJS/Expressions_Personal/js/script.js
+++ AssociatingJS/Expressions_Personal/js/script.js
@@ -4,7 +4,14 @@
 //Expressions Personal
 //How Much Do We Spend On Pizza Yearly
 
-var pizzaPrice = prompt("How mush for each pizza", 5.00);
+//Price of per pizza pie
+var pizzaPrice = prompt("How mush for each pizza?", 5.00);
 console.log(pizzaPrice);
-var pizzaNumber = prompt("How many pizzas order?",2)
+
+//# ordered
+var pizzaNumber = prompt("How many pizzas ordered per week?",2)
 console.log(pizzaNumber);
+
+//Average # of weeks pizza is ordered
+var weeksOrdered = prompt("What is the average number of weeks pizza is ordered?", 20);
+console.log(weeksOrdered);
