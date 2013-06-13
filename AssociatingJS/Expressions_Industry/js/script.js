8b860cb4e19cb73c962a993f4ce8b121cf480a2a
Juan Mason <jemason@fullsail.edu>
2013-06-13 12:31:17 -0400

    Add Your Phone Price and check in console log

--- AssociatingJS/Expressions_Industry/js/script.js
+++ AssociatingJS/Expressions_Industry/js/script.js
@@ -7,8 +7,8 @@ var videoPrice = prompt("How mush do you pay for video per month?", 59.95);
 var yourVideoPrice = videoPrice
 console.log(yourVideoPrice);
 var phonePrice = prompt("How mush do you pay for phone per month?", 49.95);
-
-
+var yourPhonePrice = phonePrice
+console.log(yourPhonePrice);
 
 
 
