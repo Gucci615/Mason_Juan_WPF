b8eacf6b2f3ef81d3fabba87a99a5280d655fdad
Juan Mason <jemason@fullsail.edu>
2013-06-13 20:57:26 -0400

    Adjust array numbers

--- AssociatingJS/Expressions_Wacky/js/script.js
+++ AssociatingJS/Expressions_Wacky/js/script.js
@@ -11,22 +11,24 @@ weightArray[0] = "149 lbs or less"
 weightArray[1] = "150 to 199 lbs";
 weightArray[2] = "200 to 349 lbs";
 weightArray[3] = "350 + lbs";
-myArray[4] = "Light Weight"
-myArray[5] = "High Flyer";
-myArray[6] = "Wrestling Technician & Power House";
-myArray[7] = "Super Heavy Weight";
-
 
 //Your Weight
 var weight = prompt("If your weight is equal or less than 149 lbs enter 0. Enter 1 if your weight no mre than 199 and no less than 150 lbs. Enter 2 if your weight no mre than 349 and no less than 200 lbs. Enter 3 if your weight 350 lbs or more", 1);
 
 console.log("Your curent weight is " + weightArray[weight] + ".");
 
+var  styleArray = new Array1();
+styleArray[0] = "Light Weight"
+styleArray[1] = "High Flyer";
+styleArray[2] = "Wrestling Technician & Power House";
+styleArray[3] = "Super Heavy Weight";
+
+
 //Pick a wrestling style
 
-var style = prompt("Select your wrestling style: 4 for Light Weight, 5 for High Flyer, 6 for Wrestling Technician & Power House, or 7 for Super Heavy Weight", 4);
+var style = prompt("Select your wrestling style: 0 for Light Weight, 1 for High Flyer, 2 for Wrestling Technician & Power House, or 3 for Super Heavy Weight", 1);
 
-console.log("You have a " + myArray[style] + " wrestling style.");
+console.log("You have a " +styleArray[style] + " wrestling style.");
 
 //Wrestlers
 var wrestlersName = parseInt(weight) + parseInt(style)
