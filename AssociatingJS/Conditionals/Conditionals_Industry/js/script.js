3d020a76f5166f1c814d8c8919241ac43447c664
Juan Mason <jemason@fullsail.edu>
2013-06-20 13:45:58 -0400

    Print total invested to console log

--- AssociatingJS/Conditionals/Conditionals_Industry/js/script.js
+++ AssociatingJS/Conditionals/Conditionals_Industry/js/script.js
@@ -11,7 +11,11 @@ var moneyJuan = prompt("How much money has Juan invested?",50)
 	moneyMarlowMom = prompt("How much money has Marlow's Mom invested?",50)
 	moneyOther = prompt("Any additional money invested?",0);
 	
-var totalMony = (parseInt(moneyJuan) + parseInt(moneyMarlow) + parseInt(moneyJuanMom) + parseInt(moneyJuanDad) + parseInt(moneyMarlowMom) + parseInt(moneyOther));
+// Calc total of all money invested by investores	
+var totalMoney = (parseInt(moneyJuan) + parseInt(moneyMarlow) + parseInt(moneyJuanMom) + parseInt(moneyJuanDad) + parseInt(moneyMarlowMom) + parseInt(moneyOther));
+// Print total invested
+console.log("The total amont invested is $" + totalMoney + ".");
+
 
 
 
