7129cdf4a4cadf5298ed40425e11f1b61e16a78b
Juan Mason <jemason@fullsail.edu>
2013-06-20 12:13:42 -0400

    Alert -  do we have enough $

--- AssociatingJS/Conditionals/Conditionals_Industry/js/script.js
+++ AssociatingJS/Conditionals/Conditionals_Industry/js/script.js
@@ -2,6 +2,8 @@
 // Conditionals Industry
 // Money to print & release the JAM Mix Tape CD
 
+alert("Do we have enough money from investors to record, print and release the new JAM Mix Tape CD?");
+
 
 
 
