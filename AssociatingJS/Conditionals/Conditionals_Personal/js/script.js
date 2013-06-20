c40cb90228eb2ec621ac8c27cf62e7ff5a26e65e
Juan Mason <jemason@fullsail.edu>
2013-06-19 20:10:44 -0400

    Initial Conditional Personal JS

---
+++ AssociatingJS/Conditionals/Conditionals_Personal/js/script.js
@@ -0,0 +1,17 @@
+//Mason_Juan 06/19/13 Conditionals_Personal
+//Time Spent Exercising
+
+// Prompt time spent on exercise per day.
+var monadyTime = prompt("Enter the amount of time spent on exercise in minutes.", 90)
+
+
+
+
+
+
+
+
+
+
+
+
