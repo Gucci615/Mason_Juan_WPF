396cc6418d32de105900c7cd57931ef9f3cc11a0
Juan Mason <jemason@fullsail.edu>
2013-06-20 19:33:30 -0400

    Add new line breakse between each system example

--- AssociatingJS/Conditionals/Conditionals_Wacky/js/script.js
+++ AssociatingJS/Conditionals/Conditionals_Wacky/js/script.js
@@ -8,7 +8,7 @@ alert("Are you a gamer?");
 var gameArray = new Array ("PS 3",  "Xbox 360", "Computer", "Cell Phone, Other");
 	
 	
-var system = prompt("What system to you play games on the most? PS 3, Xbox 360, Computer, Cell Phone, or Other...", (gameArray[3]));
+var system = prompt("What system to you play games on the most? \n PS 3 \n Xbox 360 \n Computer\n Cell Phone \n Other...", (gameArray[3]));
 	
 
 
