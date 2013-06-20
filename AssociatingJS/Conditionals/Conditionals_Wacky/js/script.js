24185efff2d38f68d34b8cfbf6db353dc8a6ba30
Juan Mason <jemason@fullsail.edu>
2013-06-20 18:55:54 -0400

    Add gameArrey

--- AssociatingJS/Conditionals/Conditionals_Wacky/js/script.js
+++ AssociatingJS/Conditionals/Conditionals_Wacky/js/script.js
@@ -4,6 +4,12 @@
 
 alert("Are you a gamer?");
 
+var gameArray = newArray
+	gameArray[0] = ("Playstation 3");
+	gameArray[1] = ("X Box 360");
+	gameArray[2] = ("Computer");
+	gameArray[3] = ("Cell Phone");
+	
 
 
 
