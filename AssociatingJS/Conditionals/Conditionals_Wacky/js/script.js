b78b918dbfd6a2ca55b9d319ba15324282a1b821
Juan Mason <jemason@fullsail.edu>
2013-06-20 19:57:48 -0400

    Add 1sp person shooter games

--- AssociatingJS/Conditionals/Conditionals_Wacky/js/script.js
+++ AssociatingJS/Conditionals/Conditionals_Wacky/js/script.js
@@ -11,7 +11,10 @@ var gameArray = new Array ("PS 3",  "Xbox 360", "Computer", "Cell Phone, Other")
 var systemName = prompt("What system to you play games on the most? \n PS 3 \n Xbox 360 \n Computer\n Cell Phone \n Other...", (gameArray[3]));
 
 //Type of games played most
-var gameType = prompt("What types of games do you like to play the most? \n 1st Person Shooters \n Fighting \n Sports \n Strategy", Fighting);	
+var gameType = prompt("What types of games do you like to play the most? \n 1st Person Shooters \n Fighting \n Sports \n Strategy", "Fighting");
+	shooters = ("Call of Duty, Sniper, and Battlefield");
+	
+	
 
 
 
