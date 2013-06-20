859211842fb1643c1bf860b5a92373e96291f693
Juan Mason <jemason@fullsail.edu>
2013-06-19 20:28:03 -0400

    Add Var for all the days of the week.

--- AssociatingJS/Conditionals/Conditionals_Personal/js/script.js
+++ AssociatingJS/Conditionals/Conditionals_Personal/js/script.js
@@ -2,9 +2,14 @@
 //Time Spent Exercising
 
 // Prompt time spent on exercise per day.
-var monTime = prompt("Enter the amount of time spent exercising Monday in minutes.", 90)
-	tueTime = prompt("Enter the amount of time spent exercising Tuesday in minutes.", 90)
-
+var monTime = prompt("Enter the amount of time spent exercising Monday in minutes.", 10)
+	tueTime = prompt("Enter the amount of time spent exercising Tuesday in minutes.", 10)
+	wedTime = prompt("Enter the amount of time spent exercising Wednesday in minutes.", 10)
+	thurTime = prompt("Enter the amount of time spent exercising Thursday in minutes.", 10)
+	friTime = prompt("Enter the amount of time spent exercising Frisday in minutes.", 10)
+	satTime = prompt("Enter the amount of time spent exercising Saturday in minutes.", 10)
+	sunTime = prompt("Enter the amount of time spent exercising Sunday in minutes.", 10)
+	
 
 
 
