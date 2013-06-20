94ff59c4f619f230f99b4c4de7a350d553aa6301
Juan Mason <jemason@fullsail.edu>
2013-06-20 19:22:37 -0400

    Correct newArray to new Array

--- AssociatingJS/Conditionals/Conditionals_Wacky/js/script.js
+++ AssociatingJS/Conditionals/Conditionals_Wacky/js/script.js
@@ -5,7 +5,7 @@
 alert("Are you a gamer?");
 
 // Game system array
-var gameArray = newArray("Playstation 3", "Playstation" , "PS3" , "X Box 360" , "Xbox" , 360 , "Computer" , "Cell Phone");
+var gameArray = new Array ("Playstation 3", "Playstation", "PS3", "X Box 360", "Xbox", 360, "Computer", "Cell Phone");
 	
 	
 var system = prompt("What system to you play games on the most?", (gameArray[3]));
