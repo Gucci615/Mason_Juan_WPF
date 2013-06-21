ed2e11eec1258a261f94ed177eea84f521985a60
Juan Mason <jemason@fullsail.edu>
2013-06-20 20:19:23 -0400

    Test correction to gameTypeArray and console log code

--- AssociatingJS/Conditionals/Conditionals_Wacky/js/script.js
+++ AssociatingJS/Conditionals/Conditionals_Wacky/js/script.js
@@ -11,11 +11,14 @@ var gameArray = new Array ("PS 3",  "Xbox 360", "Computer", "Cell Phone, Other")
 var systemName = prompt("What system to you play games on the most? \n PS 3 \n Xbox 360 \n Computer\n Cell Phone \n Other...", (gameArray[3]));
 
 //Type of games played most
-var gameType = prompt("What types of games do you like to play the most? \n 1st Person Shooters \n Fighting \n Sports \n Strategy", "Fighting");
-	shooters = ("Call of Duty, Sniper, and Battlefield");
-	fighting = ("Injustice, WWE, UFC, and Tekken");
-	sports = ("Madden, NBA 2K, FIFA Soccer, NHL, and MLB");
-	strategy = ("XCOM, R.U.S.E., Command & Conquer, and Greed Corp");
+
+var gameType = prompt("Select the number for the type of games you like the mosr\n 0 Shooter \n 1 Fighting \n 2 Sports \n 3 Strategy", 3);
+var gameTypeArray = new Array ("Shooter" , "Fighting" , "Sports" , "Strategy");
+	gameTypeArray[0] = ("Call of Duty, Sniper, and Battlefield");
+	Fighting = ("Injustice, WWE, UFC, and Tekken");
+	Sports = ("Madden, NBA 2K, FIFA Soccer, NHL, and MLB");
+	Strategy = ("XCOM, R.U.S.E., Command & Conquer, and Greed Corp");
+console.log("Based on your selection, you may like such game titles as:" + gameTypeArray[gameType] + ".");
 	
 
 
