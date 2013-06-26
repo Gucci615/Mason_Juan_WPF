98653d590920643ac9ed7e4cfa0d814613d3ce0f
Juan Mason <jemason@fullsail.edu>
2013-06-26 18:49:34 -0400

    Prompt # of miles needed to drive.

--- AssociatingJS/Functions/Functions_Personal/js/script.js
+++ AssociatingJS/Functions/Functions_Personal/js/script.js
@@ -6,5 +6,6 @@
 //Calculate the amount of gas needed toi take a trip. Also how much it wil cost with todays average price per gallon.
 function gasForTrip (miles , milesPerGl)
 {
-	
+	miles = prompt("Enter the number of miles needed to drive for this trip.", 200);
 }
+
