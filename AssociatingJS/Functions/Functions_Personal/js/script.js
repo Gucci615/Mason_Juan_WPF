f28bda080bd91cac44e4da465971717aa58d203a
Juan Mason <jemason@fullsail.edu>
2013-06-26 18:59:40 -0400

    Console log miles

--- AssociatingJS/Functions/Functions_Personal/js/script.js
+++ AssociatingJS/Functions/Functions_Personal/js/script.js
@@ -4,9 +4,12 @@
 
 
 //Calculate the amount of gas needed toi take a trip. Also how much it wil cost with todays average price per gallon.
+
+
 function gasForTrip (miles , milesPerGl)
 {
 	miles = prompt("Enter the number of miles needed to drive for this trip.", 200);
 	milesPerGl = prompt("Enter the number of MPGs you vehicle can travel.", 18);
+	console.log(miles);
 }
 
