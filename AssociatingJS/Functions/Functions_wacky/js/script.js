dca8125a8294b9533d5bec7693e47fe3a813ff8a
Juan Mason <jemason@fullsail.edu>
2013-06-27 19:49:11 -0400

    Declare WCW & WWE World and call function

--- AssociatingJS/Functions/Functions_wacky/js/script.js
+++ AssociatingJS/Functions/Functions_wacky/js/script.js
@@ -3,3 +3,12 @@
 // How much have I invested in y WWE / WWF /WCW / ECW Wrestling Belts
 
 console.log("Who's the bigger wrestling fan? You or ME!");
+
+function belts(wcwWorld, wweWorld, wwfHeavy, wwfIC, wwfLight, ecdHeavy, wweSpUS, wweUS, wweTag, wwfHardCore, wwfSkul )
+{
+var wcwWorld = 299;
+var wweWorld = 349 *2;
+console.log(wweWorld);
+}
+
+belts();
