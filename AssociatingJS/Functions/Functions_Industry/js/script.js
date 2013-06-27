2c3b6cd10dd0b14cdec281c6c4f7f73ac05c15ef
Juan Mason <jemason@fullsail.edu>
2013-06-27 19:32:50 -0400

    Add in else if statement

--- AssociatingJS/Functions/Functions_Industry/js/script.js
+++ AssociatingJS/Functions/Functions_Industry/js/script.js
@@ -18,11 +18,20 @@ console.log("It will requier " + allCad5 + " feet of Cad-5 network cable to run 
 var cableOnHand = prompt("How many feet of Cad-5 do you have on hand?", 200);
 console.log("You stated you have " + cableOnHand + " feet of cable on hand.")
 
-if (cableOnHand > allCad5){ 
+if (cableOnHand > allCad5)
+{ 
 	console.log("That is " + (cableOnHand - allCad5) + " feet over what is needed.");
 	console.log("You have more than enough Cad-5 cable for this installation job.")
 }
+else if(cableOnHand = allCad5)
+{
+console.log("Cool! You have just enough Cad-5 cable for this installation job.")
+}
+
 else {
 	console.log("That is " + (allCad5 - cableOnHand) + " feet short of what is needed.");
 	console.log("Sorry, you dont have enough cable for this installation.");
 }
+
+
+
