97ef495043f740174a9d0020d47d7934c7dcef87
Juan Mason <jemason@fullsail.edu>
2013-06-27 19:15:21 -0400

    Add else statment for not having enough cable

--- AssociatingJS/Functions/Functions_Industry/js/script.js
+++ AssociatingJS/Functions/Functions_Industry/js/script.js
@@ -19,3 +19,6 @@ var cableOnHand = prompt("How many feet of Cad-5 do you have on hand?", 200);
 
 if (cableOnHand >= allCad5){ console.log("You have enough Cad-5 cable for this installation job.")
 }
+else {
+	console.log("Sorry, you dont have enough cable for this installation.")
+}
