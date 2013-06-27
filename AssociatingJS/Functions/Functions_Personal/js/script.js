57388ed30a7d43d80442da341acf64aa0936449e
Juan Mason <jemason@fullsail.edu>
2013-06-26 21:50:02 -0400

    Remove parameter and add prompt for gasPrice

--- AssociatingJS/Functions/Functions_Personal/js/script.js
+++ AssociatingJS/Functions/Functions_Personal/js/script.js
@@ -6,10 +6,11 @@
 //Calculate the amount of gas needed toi take a trip. Also how much it wil cost with todays average price per gallon.
 
 
-function gasForTrip (totalGas)
+function gasForTrip ()
 {
 	miles = prompt("Enter the number of miles needed to drive for this trip.", 200);
 	milesPerGl = prompt("Enter the number of MPGs you vehicle can travel.", 18);
+	gasPrice = prompt("Enter the average gas price per gallon.", 3.45);
 	console.log(miles);
 	console.log(milesPerGl);
 	totalGas = (miles / milesPerGl);
