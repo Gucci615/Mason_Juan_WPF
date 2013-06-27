146f49eb472a6bcf789b470df38caa92ed1114b1
Juan Mason <jemason@fullsail.edu>
2013-06-27 18:46:13 -0400

    Add inCad5 cable lenth for each camer and dvr.

--- AssociatingJS/Functions/Functions_Industry/js/script.js
+++ AssociatingJS/Functions/Functions_Industry/js/script.js
@@ -3,9 +3,11 @@
 
 //How much Cat-5 Network Cable needed to Run 8 CCTV Cameras.
 
+var cad5(20,35,54,72,30,28,44,80,2);
+
 var cad5 = function(cam1, cam2, cam3, cam4, cam5, cam6, cam7, cam8, dvr)
 {
 	var allCad5 = (cam1 + cam2 + cam3 + cam4 + cam5 + cam6 + cam7 + cam8 + dvr)
 }
 
-cad5();
+
