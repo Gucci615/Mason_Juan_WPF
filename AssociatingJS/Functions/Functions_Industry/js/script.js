900caba1ae94398296186f98a89c6fce2d1b5cc1
Juan Mason <jemason@fullsail.edu>
2013-06-27 19:07:32 -0400

    Prompt how much Cad-5 cable on hand.

--- AssociatingJS/Functions/Functions_Industry/js/script.js
+++ AssociatingJS/Functions/Functions_Industry/js/script.js
@@ -14,3 +14,5 @@ var cad5 = function(cam1, cam2, cam3, cam4, cam5, cam6, cam7, cam8, dvr)
 var allCad5 = cad5(20,35,54,72,30,28,44,80,5);
 
 console.log("It will requier " + allCad5 + " feet of Cad-5 network cable to run the 8 CCTV Cameras and DVR.");
+
+var cableOnHand = prompt("How many feet of Cad-5 do you have on hand?", 200);
