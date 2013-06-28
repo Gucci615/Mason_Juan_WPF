811855e35b150941666dd7e245826bc009bd1298
Juan Mason <jemason@fullsail.edu>
2013-06-27 20:10:08 -0400

    Add programer notes

--- AssociatingJS/Functions/Functions_wacky/js/script.js
+++ AssociatingJS/Functions/Functions_wacky/js/script.js
@@ -3,6 +3,8 @@
 // How much have I invested in y WWE / WWF /WCW / ECW Wrestling Belts
 
 console.log("Who's the bigger wrestling fan? You or ME!");
+
+//Define var to have a place for it to return to from within the function.
 var myBeltsPrice = belts();
 
 function belts(wcwWorld, wweWorld, wwfHeavy, wwfIC, wwfLight, ecdHeavy, wweSpUS, wweUS, wweTag, wwfHardCore, wwfSkul )
@@ -21,9 +23,9 @@ var wwfSkul = 189 + 115;
 
 var myBeltsPrice = (wcwWorld + wweWorld + wwfHeavy + wwfIC + wwfLight + ecdHeavy + wweSpUS + wweUS + wweTag + wwfHardCore + wwfSkul);
 return myBeltsPrice;
-
-
 }
 
+//Call function
 belts();
 console.log(myBeltsPrice);
+
